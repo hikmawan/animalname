cc2c5166732df34296fb6c675dd4a189b6dbc755
Ghani Hikmawan <ghani@paxel.co>
2020-04-29 16:13:01 +0700

    listview layout design

---
+++ app/src/main/java/dev/ghani/learnanimalname/Animal.kt
@@ -0,0 +1,13 @@
+package dev.ghani.learnanimalname
+
+class Animal {
+    var name:String?=null
+    var des:String?=null
+    var image:Int?=null
+
+    constructor(name:String, des:String, image:Int){
+        this.name=name
+        this.des=des
+        this.image=image
+    }
+}
